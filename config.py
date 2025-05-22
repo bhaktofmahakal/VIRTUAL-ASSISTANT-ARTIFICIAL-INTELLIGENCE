70fb643ca84f1111ec31f67d97411a25ead7c261
bhaktofmahakal <utsavmishraa005@gmail.com>
2025-05-22 12:46:24 +0530

    yt

---
+++ config.py
@@ -0,0 +1,8 @@
+API_CREDENTIALS = {
+    "twitter": {
+        "api_key": "आपकी_API_KEY",
+        "api_secret": "आपकी_API_SECRET",
+        "access_token": "आपका_ACCESS_TOKEN",
+        "access_token_secret": "आपका_ACCESS_TOKEN_SECRET"
+    }
+} 
